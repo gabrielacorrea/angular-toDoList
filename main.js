97e12d5a94a9cf22d580a150f529b7ae5e3cc2a6
gabrielacorrea <gabriela01sc@gmail.com>
2015-10-20 10:03:02 -0200

    Functions - constructor invocation

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
 var toDoList = [{
+	isDone: false,
 	date: "2015-09-14",
 	description: "Estudar Angular",
 	priority: 2, 
 	authors: {
 		name: 'Tay', 
 		age: 25
+	},
+	changeForDone: function() {
+		this.isDone = true;
 	}
+
 },
 {
+	isDone: false,
 	date: "2015-09-15",
 	description: "Cozinhar uma torta",
-	priority: 3, 
+	priority: 3,
 	authors: {
 		name: 'Duarte', 
 		age: 22
@@ -22,8 +28,15 @@ function list() {
 	for (toDo in toDoList) {
 		document.writeln(toDoList[toDo].description);
 	}
-
-	delete toDoList[0].description;
 };
 
+function printDone() {
+	document.writeln(toDoList[0].isDone);
+	toDoList[0].changeForDone();
+	document.writeln(toDoList[0].isDone);
+}
+
+
+
 list();
+printDone();
